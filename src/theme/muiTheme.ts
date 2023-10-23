a60dbeca8ead160de46887f55c9d0c66b18c640e
Gr33nBean <91081335+Gr33nBean@users.noreply.github.com>
2023-10-24 04:54:29 +0700

    add font Roboto
    Poppins hk hỗ trợ Tiếng Việt

--- src/theme/muiTheme.ts
+++ src/theme/muiTheme.ts
@@ -1,5 +1,15 @@
 import { PaletteMode, ThemeOptions } from "@mui/material";
-
+import "@fontsource/roboto";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/300-italic.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/400-italic.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/500-italic.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/700-italic.css";
+import "@fontsource/roboto/900.css";
+import "@fontsource/roboto/900-italic.css";
 const TopicColor = "#00404e";
 const SubColor = "#ffe6d4";
 
@@ -52,5 +62,29 @@ export const getMode = (mode: PaletteMode): ThemeOptions => ({
         },
       },
     },
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: TopicColor,
+        },
+      },
+    },
+    MuiCssBaseline: {
+      styleOverrides: `
+        @font-face {
+          font-family: 'Roboto';
+          font-style: normal;
+          font-display: swap;
+          unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+        }
+      `,
+    },
+  },
+  typography: {
+    fontFamily: ["Roboto", "sans-serif"].join(","),
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
   },
 });
