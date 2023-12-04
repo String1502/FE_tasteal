cad6bd58146cfb10cc39b608a29763989f6f5046
Gr33nBean <91081335+Gr33nBean@users.noreply.github.com>
2023-12-04 11:05:51 +0700

    Update CheckSignIn.tsx

--- src/components/ui/app/CheckSignIn.tsx
+++ src/components/ui/app/CheckSignIn.tsx
@@ -6,7 +6,7 @@ function CheckSignIn({ children }: { children: React.ReactNode }) {
   const { login } = useContext(AppContext);
   return (
     <>
-      {login.isUserSignedIn && { children }}
+      {login.isUserSignedIn && <>{children}</>}
       {login.isUserSignedIn == false && <SignInFirst />}
       {login.isUserSignedIn == undefined && <h1>loading</h1>}
     </>
