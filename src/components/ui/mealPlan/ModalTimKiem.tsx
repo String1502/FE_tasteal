beb72ad985b407a83af214b7de0d712251743c86
Gr33nBean <91081335+Gr33nBean@users.noreply.github.com>
2023-12-19 12:55:26 +0700

    move dialog file

--- src/components/ui/mealPlan/ModalTimKiem.tsx
+++ src/components/ui/mealPlan/ModalTimKiem.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@mui/material';
-import { CustomModal } from './CustomModal';
+import { CustomDialog } from '../../common/dialog/CustomDialog';
 import { useSearchRecipe } from '../search/useSearchRecipe';
 import { SearchTextField } from '../search/SearchTextField';
 import { SearchInfiniteScroll } from '../search/SearchInfiniteScroll';
@@ -9,65 +9,61 @@ import { RecipeEntity } from '@/lib/models/entities/RecipeEntity/RecipeEntity';
 const viewportItemAmount = 6;
 
 export function ModalTimKiem({
-    open,
-    handleClose,
-    title,
+  open,
+  handleClose,
+  title,
 }: {
-    open: boolean;
-    handleClose: () => void;
-    title: string;
+  open: boolean;
+  handleClose: () => void;
+  title: string;
 }) {
-    const {
-        recipes,
-        // filter,
-        // handleChangeFilter,
-        // tuKhoas,
-        // handleChangeTuKhoa,
-        textSearch,
-        handleChangeTextSearch,
-        loadNext,
-        end,
-    } = useSearchRecipe(viewportItemAmount);
-    return (
-        <CustomModal
-            open={open}
-            handleClose={handleClose}
-            title={title}
-        >
-            <Box sx={{ width: '100%', py: 2 }}>
-                <SearchTextField
-                    textSearch={textSearch}
-                    handleChangeTextSearch={handleChangeTextSearch}
-                    props={{
-                        size: 'small',
-                    }}
-                />
-            </Box>
+  const {
+    recipes,
+    // filter,
+    // handleChangeFilter,
+    // tuKhoas,
+    // handleChangeTuKhoa,
+    textSearch,
+    handleChangeTextSearch,
+    loadNext,
+    end,
+  } = useSearchRecipe(viewportItemAmount);
+  return (
+    <CustomDialog open={open} handleClose={handleClose} title={title}>
+      <Box sx={{ width: '100%', py: 2 }}>
+        <SearchTextField
+          textSearch={textSearch}
+          handleChangeTextSearch={handleChangeTextSearch}
+          props={{
+            size: 'small',
+          }}
+        />
+      </Box>
 
-            <SearchInfiniteScroll
-                loadNext={loadNext}
-                dataLenght={recipes.length}
-                end={end}
+      <SearchInfiniteScroll
+        loadNext={loadNext}
+        dataLenght={recipes.length}
+        end={end}
+      >
+        {recipes
+          .filter((item) => item)
+          .map((item, index) => (
+            <Box
+              key={item.id}
+              sx={{
+                flexBasis: {
+                  xs: '100%',
+                  sm: 'calc(99.2%/2)',
+                  md: 'calc(99.3%/3)',
+                },
+                p: 1,
+                mr: index != recipes.length - 1 ? 0 : 'auto',
+              }}
             >
-                {recipes
-                    .filter((item) => item)
-                    .map((item, index) => (
-                        <Box
-                            key={item.id}
-                            sx={{
-                                flexBasis: {
-                                    xs: '100%',
-                                    sm: 'calc(99.2%/2)',
-                                    md: 'calc(99.3%/3)',
-                                },
-                                p: 1,
-                                mr: index != recipes.length - 1 ? 0 : 'auto',
-                            }}
-                        >
-                            <CustomCardMealPlan recipe={item as RecipeEntity} />
-                        </Box>
-                    ))}
-            </SearchInfiniteScroll>
-        </CustomModal>
-    );
+              <CustomCardMealPlan recipe={item as RecipeEntity} />
+            </Box>
+          ))}
+      </SearchInfiniteScroll>
+    </CustomDialog>
+  );
 }
